b334c9a8040749f65acddac5148d94cd444a3ba5
Sarah <184196481+Sarah-Tuemay@users.noreply.github.com>
2025-05-05 23:16:09 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,25 @@
-const logo = document.querySelector(".logo");
-
+const logo = document.querySelector('.logo');
 const signUp = document.querySelector(".sign-up");
 const signIn = document.querySelector(".sign-in");
 const loginForm = document.querySelector(".loginForm");
 const signUpForm = document.querySelector(".sign-up-form");
 const signInForm = document.querySelector(".sign-in-form");
 
+const signInButton = document.querySelector('.signInButton');
+const signUpButton = document.querySelector('.signUpButton');
+
+signInButton.addEventListener('click',()=>{
+    window.location = 'jobs.html';
+})
+
+signUpButton.addEventListener('click',()=>{
+    window.location = 'jobs.html';
+})
+
+
 logo.addEventListener('click', ()=>{
     window.location.href = 'index.html';
 })
-
 signUp.addEventListener("click", function() {
     signUpForm.style.display = 'block';
     signInForm.style.display = 'none';
@@ -17,6 +27,7 @@ signUp.addEventListener("click", function() {
     signIn.classList.remove('clicked');
     loginForm.innerHTML = signUpForm.innerHTML;
 })
+
 
 signIn.addEventListener("click", function() {
     signUp.classList.remove('clicked'); 
